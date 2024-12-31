75fc51c0e83f662c497d2448aa49a30fe98e9d82
Stefan de Lange <langestefan@msn.com>
2024-12-31 20:20:33 +0100

    Add constants module

--- src/PVSimBase.jl
+++ src/PVSimBase.jl
@@ -1,5 +1,9 @@
 module PVSimBase
 
+# Interfaces
 include("Location/location.jl")
+
+# Constants
+include("./constants.jl")
 
 end
---
+++ src/constants.jl
@@ -0,0 +1,8 @@
+export JD_J2000
+
+"""
+    const JD_J2000
+
+Julian Day of J2000.0 epoch (2000-01-01T12:00:00.000).
+"""
+const JD_J2000 = 2451545.0
